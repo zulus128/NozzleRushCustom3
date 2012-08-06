bc4cd7fbe0fd3792321408c3945b00139966e9d4
Abdul Jaleel Malik <abduljaleelmalik@Lions-Mac.local>
2012-08-06 12:04:40 +0400

    12340

--- NozzleRush/GameScene.mm
+++ NozzleRush/GameScene.mm
@@ -105,20 +105,21 @@ enum {
         [[[Common instance].tileMap layerNamed:@"BackBackgroundLayer2"] setZOrder:-20];
         [[[Common instance].tileMap layerNamed:@"BackBackgroundLayer3"] setZOrder:-19];
         [[[Common instance].tileMap layerNamed:@"BackBackgroundLayer4"] setZOrder:-18];
-        [[[Common instance].tileMap layerNamed:@"BackBackgroundLayer5"] setZOrder:-17];
-        [[[Common instance].tileMap layerNamed:@"BackBackgroundLayer6"] setZOrder:-16];
+//        [[[Common instance].tileMap layerNamed:@"BackBackgroundLayer5"] setZOrder:-17];
+//        [[[Common instance].tileMap layerNamed:@"BackBackgroundLayer6"] setZOrder:-16];
         
         [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer1"] setZOrder:-15];
         [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer2"] setZOrder:-14];
         [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer3"] setZOrder:-13];
         [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer4"] setZOrder:-12];
-        [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer5"] setZOrder:-11];
-        [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer6"] setZOrder:-10];
+//        [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer5"] setZOrder:-11];
+//        [[[Common instance].tileMap layerNamed:@"FrontBackgroundLayer6"] setZOrder:-10];
 
         [[[Common instance].tileMap layerNamed:@"FrontBackgroundObjects"] setZOrder:-9];
         
         [[[Common instance].tileMap layerNamed:@"ColumnLayer"] setZOrder:-8];
-        [[[Common instance].tileMap layerNamed:@"CopyRoadLayer"] setZOrder:-7];
+//        [[[Common instance].tileMap layerNamed:@"CopyRoadLayer"] setZOrder:-7];
+        [[[Common instance].tileMap layerNamed:@"RoadLayer shifted"] setZOrder:-7];
         [[[Common instance].tileMap layerNamed:@"RoadLayer"] setZOrder:-6];
         [[[Common instance].tileMap layerNamed:@"RazmetkaLayer"] setZOrder:-5];
         [[[Common instance].tileMap layerNamed:@"BackBorderLayer_1"] setZOrder:-4];
@@ -126,7 +127,8 @@ enum {
 
         [[[Common instance].tileMap layerNamed:@"TrackObjectsLayerLow"] setZOrder:-3];
         [[[Common instance].tileMap layerNamed:@"TrackObjectsLayer"] setZOrder:-2];
-        [[[Common instance].tileMap layerNamed:@"TrackObjectsLayerHigh"] setZOrder:-1];
+//        [[[Common instance].tileMap layerNamed:@"TrackObjectsLayerHigh"] setZOrder:-1];
+        [[[Common instance].tileMap layerNamed:@"TrackObjectsLayerUpper"] setZOrder:-1];
 
         //zOrder:0 for cars
         [[[Common instance].tileMap layerNamed:@"FrontBorderLayer_2"] setZOrder:1];
